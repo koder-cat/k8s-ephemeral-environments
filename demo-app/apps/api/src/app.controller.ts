6cde314a9078fc43efe5af3f7edf1868d55fd7b5
genesluna <genesluna@gmail.com>
2025-12-29 21:01:27 -0300

    test: validate preserve feature

--- demo-app/apps/api/src/app.controller.ts
+++ demo-app/apps/api/src/app.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+// Test PR for preserve feature validation
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
